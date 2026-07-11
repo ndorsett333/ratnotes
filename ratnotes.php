8b0574ad3fc532156c63b47c0e3a2ea26c76ef80
ndorsett333 <nicolasdorsett@gmail.com>
2026-07-11 14:27:27 -0600

    up version

--- ratnotes.php
+++ ratnotes.php
@@ -3,7 +3,7 @@
  * Plugin Name:       RatNotes
  * Plugin URI:        https://example.com/ratnotes
  * Description:       A Google Keep-like note-taking application for WordPress.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 5.8
  * Requires PHP:      7.4
  * Author:            Nick Dorsett
